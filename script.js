54bbe959d720cd8d042beaced631c3928d560e10
Derek Ngan <dd_derek129@hotmail.com>
2023-03-11 20:05:21 -0800

    added animations for hamburger menu, header title and properly placed contact section content

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const sunEl = document.getElementById('sun');
 const moonEl = document.getElementById('moon');
 const bodyEl = document.getElementById('body');
 const iconEl = document.getElementById('icons');
+const inputNameEl = document.getElementById('name');
+const inputEmailEl = document.getElementById('email');
+const textAreaEl = document.getElementById('message');
+const burgerBarOneEl = document.getElementById('burgerBarOne');
+const burgerBarTwoEl = document.getElementById('burgerBarTwo');
+const burgerBarThreeEl = document.getElementById('burgerBarThree');
+
 
 // button variables
 const sliderEl = document.getElementById('slider');
@@ -11,12 +18,28 @@ const slideEl = document.getElementById('slide');
 const nextEl = document.getElementById('slideNext');
 const prevEl = document.getElementById('slidePrev');
 
+// hamburger variables
+const hamburgerMenu = document.getElementById("hamburgerMenu");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("navOne");
+const nav2 = document.getElementById("navTwo");
+const nav3 = document.getElementById("navThree");
+const nav4 = document.getElementById("navFour");
+const nav5 = document.getElementById("navFive");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
 // darkmode function
 containerEl.addEventListener('click', function(){
     sunEl.classList.toggle('animate-sun');
     moonEl.classList.toggle('animate-moon');
-    bodyEl.classList.toggle('lightmode');
-    iconEl.classList.toggle('lightmode');
+    bodyEl.classList.toggle('darkmode');
+    iconEl.classList.toggle('darkmode');
+    inputNameEl.classList.toggle('darkmode');
+    inputEmailEl.classList.toggle('darkmode');
+    textAreaEl.classList.toggle('darkmode');
+    burgerBarOneEl.classList.toggle('darkmode');
+    burgerBarTwoEl.classList.toggle('darkmode');
+    burgerBarThreeEl.classList.toggle('darkmode');
 });
 
 // carousel function
@@ -30,4 +53,36 @@ prevEl.addEventListener('click', () => {
     sliderEl.scrollLeft -= slideWidth;
 });
 
-
+// Hamburger animation functions
+function navAnimation(val1, val2) {
+    navItems.forEach((nav, i) => {
+      nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+    });
+  }
+  function toggleNav() {
+    // Toggle: Hamburger Open/Close
+    hamburgerMenu.classList.toggle("active");
+  
+    //   Toggle: Menu Active
+    overlay.classList.toggle("overlay-active");
+  
+    if (overlay.classList.contains("overlay-active")) {
+      // Animate In - Overlay
+      overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+  
+      // Animate In - Nav Items
+      navAnimation("out", "in");
+    } else {
+      // Animate Out - Overlay
+      overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+  
+      // Animate Out - Nav Items
+      navAnimation("in", "out");
+    }
+  }
+  
+  // Events Listeners
+  hamburgerMenu.addEventListener("click", toggleNav);
+  navItems.forEach((nav) => {
+    nav.addEventListener("click", toggleNav);
+  });
